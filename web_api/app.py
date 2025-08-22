8653952fb4c2c16f376c1dc124493287d1af9177
Abdallah Divkar <abdallahdivker@gmail.com>
2025-08-22 23:12:34 +0300

    Tkinter .exe deployment

--- web_api/app.py
+++ web_api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import sys
 import os
@@ -16,6 +16,14 @@ def voice():
         response.append(txt)
     smart_response(text, capture_output, tts=False)
     return jsonify({"response": response})
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
